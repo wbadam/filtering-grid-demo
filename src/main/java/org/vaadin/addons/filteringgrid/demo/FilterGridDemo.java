d689a9732c53e2a9a4615932be25991d42f6dc91
wbadam <wagadam@gmail.com>
2018-05-14 15:18:16 +1000

    Create demo application

---
+++ src/main/java/org/vaadin/addons/filteringgrid/demo/FilterGridDemo.java
@@ -0,0 +1,79 @@
+package org.vaadin.addons.filteringgrid.demo;
+
+import javax.servlet.annotation.WebServlet;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+
+import org.vaadin.addons.filteringgrid.FilterGrid;
+import org.vaadin.addons.filteringgrid.comparators.Comparator;
+import org.vaadin.addons.filteringgrid.comparators.StringComparator;
+import org.vaadin.addons.filteringgrid.components.DateRangeField;
+import org.vaadin.addons.filteringgrid.demo.data.Person;
+import org.vaadin.addons.filteringgrid.demo.data.Person.Continent;
+import org.vaadin.addons.filteringgrid.demo.data.PersonService;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.provider.DataProvider;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.DateField;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.renderers.DateRenderer;
+
+@Theme("mytheme")
+public class FilterGridDemo extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+
+        FilterGrid<Person> grid = new FilterGrid<>();
+        grid.setWidth("100%");
+
+        FilterGrid.Column<Person, Integer> colIndex = grid.addColumn(Person::getIndex);
+        FilterGrid.Column<Person, String> colFirstName = grid.addColumn(Person::getFirstName);
+        FilterGrid.Column<Person, String> colLastName = grid.addColumn(Person::getLastName);
+        FilterGrid.Column<Person, Date> colDateOfBirth = grid.addColumn(Person::getDateOfBirth, new DateRenderer("%1$tB %1$te, %1$tY", Locale.ENGLISH));
+        FilterGrid.Column<Person, String> colAddress = grid.addColumn(Person::getAddress);
+        FilterGrid.Column<Person, Double> colLatitude = grid.addColumn(Person::getLatitude);
+        FilterGrid.Column<Person, Double> colLongitude = grid.addColumn(Person::getLongitude);
+        FilterGrid.Column<Person, Continent> colContinent = grid.addColumn(Person::getContinent);
+        FilterGrid.Column<Person, String> colEmail = grid.addColumn(Person::getEmail);
+        FilterGrid.Column<Person, String> colPhone = grid.addColumn(Person::getPhone);
+        FilterGrid.Column<Person, String> colCompany = grid.addColumn(Person::getCompany);
+        FilterGrid.Column<Person, Float> colBalance = grid.addColumn(Person::getBalance);
+        FilterGrid.Column<Person, Date> colRegistered = grid.addColumn(Person::getRegistered);
+        FilterGrid.Column<Person, Boolean> colActive = grid.addColumn(Person::isActive);
+        FilterGrid.Column<Person, String> colGuid = grid.addColumn(Person::getGuid);
+
+        colFirstName.setFilter(new TextField(), StringComparator.containsIgnoreCase());
+        colLastName.setFilter(new TextField(), StringComparator.containsIgnoreCase());
+        colDateOfBirth.setFilter(v -> v.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), new DateField(), (v, fv) -> fv == null || v.isEqual(fv));
+        colAddress.setFilter(new TextField(), StringComparator.containsIgnoreCase());
+        colRegistered.setFilter(v -> v.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), new DateRangeField(), (v, fv) -> fv == null || (!v.isBefore(fv.getDateFrom()) && !v.isAfter(fv.getDateTo())));
+        colEmail.setFilter(new TextField(), StringComparator.containsIgnoreCase());
+        colPhone.setFilter(new TextField(), StringComparator.containsIgnoreCase());
+        colCompany.setFilter(new TextField(), StringComparator.containsIgnoreCase());
+        colBalance.setFilter(new TextField(), (v, fv) -> fv.isEmpty() || v < Float.valueOf(fv));
+        colContinent.setFilter(new ComboBox<>(null, Arrays.asList(Continent.values())), Comparator.equals());
+        colActive.setFilter(new ComboBox<>(null, Arrays.asList(true, false)), Comparator.equals());
+
+        grid.setFilteredDataProvider(DataProvider
+                .ofCollection(PersonService.getInstance().getAll()));
+
+        layout.addComponent(grid);
+
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "DemoUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = FilterGridDemo.class, productionMode = false)
+    public static class DemoUIServlet extends VaadinServlet {
+    }
+}
---
+++ src/main/java/org/vaadin/addons/filteringgrid/demo/data/PersonService.java
@@ -0,0 +1,138 @@
+package org.vaadin.addons.filteringgrid.demo.data;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import elemental.json.JsonArray;
+import elemental.json.JsonObject;
+import elemental.json.impl.JsonUtil;
+import org.apache.commons.io.FileUtils;
+import org.vaadin.addons.filteringgrid.comparators.StringComparator;
+
+public class PersonService {
+
+    private static PersonService instance;
+    private final List<Person> people;
+
+    private PersonService() {
+        people = fetchAll();
+    }
+
+    public static PersonService getInstance() {
+        if (instance == null) {
+            instance = new PersonService();
+        }
+        return instance;
+    }
+
+    private Predicate<Person> filterFirstName(String filter) {
+        return person -> StringComparator.containsIgnoreCase().test(person
+                .getFirstName(), filter);
+    }
+
+    private Predicate<Person> filterLastName(String filter) {
+        return person -> StringComparator.containsIgnoreCase().test(person
+                .getLastName(), filter);
+    }
+
+    private Predicate<Person> filterName(String filter) {
+        return person -> StringComparator.containsIgnoreCase().test(person
+                .getFirstName() + " " + person.getLastName(), filter);
+    }
+
+    public List<Person> getAll() {
+        return people;
+    }
+
+    public List<Person> getPersons(int offset, int limit) {
+        return people.subList(offset, offset + limit);
+    }
+
+    public Stream<Person> getPersonsFilteredFirstName(int offset, int limit,
+                                                      String firstNameContains) {
+        return people.stream().filter(filterFirstName(firstNameContains))
+                .skip(offset).limit(limit);
+    }
+
+    public Stream<Person> getPersonsFilteredName(int offset, int limit,
+                                                 String nameContains) {
+        return people.stream().filter(filterName(nameContains)).skip(offset)
+                .limit(limit);
+    }
+
+    public int getSize() {
+        return people.size();
+    }
+
+    public int getSizeFilteredFirstName(String firstNameContains) {
+        return (int) people.stream().filter(filterFirstName
+                (firstNameContains)).count();
+    }
+
+    public int getSizeFilteredName(String nameContains) {
+        return (int) people.stream().filter(filterName(nameContains)).count();
+    }
+
+    private List<Person> fetchAll() {
+
+        String dataJson = "";
+
+        try {
+            dataJson = FileUtils.readFileToString(new File(
+                    getClass().getClassLoader().getResource("data.json")
+                            .getFile()), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        SimpleDateFormat dateOfBirthFormat = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat registeredFormat = new SimpleDateFormat(
+                "E, MMMM d, yyyy hh:mm a");
+
+        List<Person> persons = new ArrayList<>();
+
+        JsonArray data = JsonUtil.parse(dataJson);
+        for (int i = 0; i < data.length(); i++) {
+            JsonObject p = data.getObject(i);
+
+            Person person = new Person();
+            person.setIndex((int) p.getNumber("index"));
+            person.setGuid(p.getString("guid"));
+
+            JsonObject name = p.getObject("name");
+            person.setFirstName(name.getString("first"));
+            person.setLastName(name.getString("last"));
+
+            try {
+                person.setDateOfBirth(
+                        dateOfBirthFormat.parse(p.getString("dateOfBirth")));
+                person.setRegistered(
+                        registeredFormat.parse(p.getString("registered")));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            person.setAddress(p.getString("address"));
+            person.setEmail(p.getString("email"));
+            person.setPhone(p.getString("phone"));
+            person.setCompany(p.getString("company"));
+            person.setLongitude(Double.valueOf(p.getString("longitude")));
+            person.setLatitude(Double.valueOf(p.getString("latitude")));
+            person.setBalance(Float.valueOf(
+                    p.getString("balance").substring(1).replaceAll(",", "")));
+            person.setActive(p.getBoolean("isActive"));
+            person.setContinent(Person.Continent.valueOf(
+                    p.getString("continent").toUpperCase().replace(' ', '_')));
+
+            persons.add(person);
+        }
+
+        return persons;
+    }
+}
